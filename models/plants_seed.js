0c2142761513b6d1815d17a5a7bbd25a8085bf68
Angelines Yaport-Garcia <angelinesyg@gmail.com>
2021-05-10 16:49:51 -0400

    Added Seed Route

---
+++ models/plants_seed.js
@@ -0,0 +1,44 @@
+module.exports = [
+   {
+      name: "Cute Cactus",
+      image: "https://images.unsplash.com/photo-1459411552884-841db9b3cc2a?ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8Y2FjdHVzfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60",
+      notes: "Angie loves Cacti, so much!",
+      season: "Summer",
+
+   },
+   {
+      name: "Heart Plant",
+      image: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/monstera-baby-plant-royalty-free-image-1133859657-1553276209.jpg",
+      notes: "Because everyone needs love",
+      season: "All season",
+
+   },
+   {
+      name: "Monstera",
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnXZb4tXNHKQ0eEr9NRZfevuCLvwKnXkwi-A&usqp=CAU",
+      notes: "Ryani loves the weird plant",
+      season: "All season, Indoor plant",
+
+   },
+   {
+      name: "Mint Plant",
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHybzQrLxfI7-U-FkoTmiDAYunQfBKTMkGSA&usqp=CAU",
+      notes: "Minty Fresh tea and/or aroma",
+      season: "Summer",
+
+   },
+   {
+      name: "Aloe Vera",
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREavESRVnR5rVqpBO4aaUlzh-7OgFx9hjZPg&usqp=CAU",
+      notes: "Healing Indoor Plant",
+      season: "Summer",
+
+   },
+   {
+      name: "Lavender",
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-6F0WXWgC9MFrWJ6ONl8qUIsBoVInFRjczAWJYLLrkJqS019zWcbllXe2BA0&usqp=CAc",
+      notes: "Beautifuly Purple Lavender",
+      season: "Summer",
+
+   },
+]
