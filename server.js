04451c79c7f5335a03561f097f50b9b3c4f3ca9a
cheisentrout <cheisentrout@gmail.com>
2021-05-10 14:12:07 -0400

    Set up basic server.js and dependencies

---
+++ server.js
@@ -0,0 +1,51 @@
+/*============ DEPENDENCIES =============*/
+
+const express = require('express')
+// const mongoose = require('mongoose')
+
+/*============ CONFIGURATION =============*/
+
+const app = express()
+// require('dotenv').config()
+// const PORT = process.env.PORT
+// const MONGODB_URI = process.env.MONGODB_URI
+
+// mongoose.connect(MONGODB_URI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   useFindAndModify: false
+// })
+
+/*============ MONGOOSE ERR/SUCCESS =============*/
+
+// mongoose.connection.on('error', err =>
+//   console.log(
+//     err.message,
+//     ' is Mongod not running?/Problem with Atlas Connection?'
+//   )
+// )
+// mongoose.connection.on('connected', () =>
+//   console.log('mongo connected: ', MONGODB_URI)
+// )
+// mongoose.connection.on('disconnected', () => console.log('mongo disconnected'))
+
+/*============= MIDDLEWARE ==============*/
+
+//Use this middleware to return JSON data, rather than res.send/urlencoded which returns HTML:
+app.use(express.json())
+app.use(express.static('public'))
+
+/*============ CONTROLLERS =============*/
+
+
+/*============ ROUTES =============*/
+
+app.get('/', (req, res) => {
+  res.send('Hello world')
+})
+
+/*============== LISTENER ================*/
+
+app.listen(3000, () => {
+  console.log('listening on port 3000');
+})
